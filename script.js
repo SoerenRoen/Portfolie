3016769c8e54cdfba6a961efaf4b6bfcad53c9ba
Søren Vindfelt Røn <SoerenRoen@laptop-vaq5u3ea.kea.local>
2019-06-18 12:24:25 +0200

    Almost validated

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("Siden Vises");
     console.log("Click");
-    document.querySelector("#mere").addEventListener("click", readMore);
+    document.querySelector(".mere").addEventListener("click", readMore);
     document.querySelector(".mere_2").addEventListener("click", readMore_2);
 }
 
 function readMore() {
     console.log("Read More");
-    document.querySelector("#box").classList.toggle("stor");
-    document.querySelector("#box").classList.toggle("lille");
+    document.querySelector(".box").classList.toggle("stor");
+    document.querySelector(".box").classList.toggle("lille");
 }
 
 function readMore_2() {
